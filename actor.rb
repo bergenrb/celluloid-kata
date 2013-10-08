7790db815d4a2254be59ea1326c4eb1c71bce77f
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2013-10-08 19:40:17 +0200

    tst

--- actor.rb
+++ actor.rb
@@ -1,5 +1,7 @@
 require 'celluloid/autostart'
 
+require './db'
+
 class Actor
   include Celluloid
 
@@ -12,13 +14,19 @@ class Actor
     Celluloid::Actor[@process_key] = Actor.current
   end
 
+  # Lookup the this process'
   def pair_process
     Celluloid::Actor[@pair_process_key] || raise
   end
 
-  def create_item
+  def start
+    create_item
   end
 
+  def create_item
+    last_id = Db.insert
+    puts "Created item with id: #{last_id}"
+  end
 end
 
 a1 = Actor.new(:a1, :a2)
@@ -26,3 +34,10 @@ a1.register
 
 a2 = Actor.new(:a2, :a1)
 a2.register
+
+a1.start
+a2.start
+
+loop do
+
+end
